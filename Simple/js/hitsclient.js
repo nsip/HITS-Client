4cb9263a2038227bb26b7bce0dc991273e019d18
Ben Carter <ben@tearsofaunicorn.com>
2017-07-11 08:42:37 +0800

    Fix Put One Bugs

--- Simple/js/hitsclient.js
+++ Simple/js/hitsclient.js
@@ -256,7 +256,7 @@ var hitsclient = (function(_app) {
 			$("#putOneBody").parent("div").removeClass("has-error");
 		}
 
-		var refid = $("putOneReferenceId").val();
+		var refid = $("#putOneReferenceId").val();
 		if (refId.length == 0) {
 			panelError("Please enter a Reference Id");
 			$("#putOneReferenceId").parent("div").addClass("has-error");
@@ -280,7 +280,7 @@ var hitsclient = (function(_app) {
 		args.url = args.url.slice(0, -1); // Strip plural provider name
 		//	args.contentType = "application/xml";
 		args.dataType = "XML";
-		args.type = "POST";
+		args.type = "PUT";
 		var token = getTokenForAuth(sessionToken, password);
 		args.token = token;
 		args.headers = {};
