14313fb82115521fc50e5bffc84e166d2858de1e
Nick Nicholas <opoudjis@optusnet.com.au>
2018-06-06 14:37:56 +1000

    rename client

--- Simple/js/providers/student-contact.js
+++ Simple/js/providers/student-contact.js
@@ -4,7 +4,7 @@ var hitsclient = (function(_app) {
   }
 
   _app.providers.push({
-    name : "Student Contact",
+    name : "Student Contact Personal",
     value : "StudentContactPersonals",
     template : 
       "<!-- Remember to insert RefIds of records that exist; and to hyphenate RefID as\n" +
