81595f2b47745a71e91f09dfa5b7b2104224c392
Scott Penrose <scottp@dd.com.au>
2020-07-17 00:42:58 +0000

    Fix student score template and plural

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Example Client access SIF data via HITS
 
 Instructions to create new endpoint:
 
-Create hits-clientSimple/js/providers/*.js
+* Create hits-clientSimple/js/providers/*.js
+* Make sure "value" has the plural name of the object
+* Add that JS file to Simple/index.js and hits-dashbaoard/right/source/jade/client.html
 
-Add that JS file to Simple/index.js and hits-dashbaoard/right/source/jade/client.html
--- Simple/js/providers/student-score-judgement-against-standard.js
+++ Simple/js/providers/student-score-judgement-against-standard.js
@@ -5,11 +5,25 @@ var hitsclient = (function(_app) {
 
   _app.providers.push({
     name : "Student Score Judgement Against Standard",
-    value : "StudentScoreJudgementAgainstStandard",
+    value : "StudentScoreJudgementAgainstStandards",
     template : 
       "<!-- Remember to insert RefIds of records that exist; and to hyphenate RefID as\n" +
       "     xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx -->\n" +
-"<StudentScoreJudgementAgainstStandard>\n" +
+"<StudentScoreJudgementAgainstStandard”>\n" +
+"  <SchoolYear>2018</SchoolYear>\n" +
+"  <TermInfoRefId>{RefId}</TermInfoRefId>\n" +
+"  <StudentPersonalRefId>{RefId}</StudentPersonalRefId>\n" +
+"  <YearLevel>\n" +
+"    <Code>10</Code>\n" +
+"  </YearLevel>\n" +
+"  <TeachingGroupRefId>{RefId}</TeachingGroupRefId>\n" +
+"  <StaffPersonalRefId>{RefId}</StaffPersonalRefId>\n" +
+"  <CurriculumCode>AUSVELS</CurriculumCode>\n" +
+"  <CurriculumNodeCode>MATNUM</CurriculumNodeCode>\n" +
+"  <Score>10</Score>\n" +
+"  <SpecialCircumstanceLocalCode>SCMP</SpecialCircumstanceLocalCode>\n" +
+"  <ManagedPathwayLocalCode>MPOMP</ManagedPathwayLocalCode>\n" +
+"  <SchoolInfoRefId>{RefId}</SchoolInfoRefId>\n" +
 "</StudentScoreJudgementAgainstStandard>"
   });
 
